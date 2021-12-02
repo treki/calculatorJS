880855c52ee21efcd806bfa412b4bb999e6492fd
treki <tuya.senff@gmail.com>
2021-12-02 13:01:56 -0600

    first commit

---
+++ calculator.js
@@ -0,0 +1,22 @@
+let result = document.getElementById('inputText');
+
+let calculate=(number)=>{
+    result.value+=number;
+}
+
+let Result=()=>{
+    try{
+        result.value=eval(result.value)
+    }
+    catch(err){
+        alert('Invalid Input');
+    }
+}
+
+function clr(){
+    result.value=" ";
+}
+
+function del(){
+    result.value=result.value.slice(0,-1);
+}
